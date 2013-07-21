a932e78d7605d0c1f2feaf72159f8a645c894827
Dmitry Bublik <dmitry.buba@gmail.com>
2013-07-21 13:44:33 +0300

    First

---
+++ VirtualPanelTest/IPageInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace VirtualPanelTest
+{
+    public interface IPageInfo
+    {
+        int PageCount { get; }
+
+        int CurrentPage { get; set; }
+
+        Rect CurrentPageRect { get; }
+
+        void BeginUpdate();
+
+        void EndUpdate();
+    }
+}
